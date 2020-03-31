cbc735125f5670fc8c6fdadaf2e9316fe7cd3a38
Wilton Leandro Polesca De Souza <wpolesca@newtrax.com>
2020-03-31 09:56:02 -0300

    Add project files.

---
+++ Aspnet.SignalIR.Course/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aspnet.SignalIR.Course.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMesageToAll(string message)
+        {
+            return Clients.All.SendAsync("SendMessage", message);
+        }
+    }
+}
---
+++ Aspnet.SignalIR.Course/wwwroot/js/message.js
@@ -0,0 +1,30 @@
+﻿'use strinct'
+
+
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/messages")
+    .build();
+
+connection.on("SendMessage", function (message) {
+
+    const msg = message.replace(/&/g, "&amp")
+        .replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    const div = document.createElement("div");
+    div.innerHTML = msg + "<hr/>";
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.start()
+    .catch(function (err) {
+        return console.log(err);
+    });
+
+document.getElementById("sendButton").addEventListener("click", function (e) {
+    const msg = document.getElementById("message").value;
+
+    // SendMesageToAll = Name of the method
+    connection.invoke("SendMesageToAll", msg).catch(function (err) {
+        console.error(err)
+    });
+});
